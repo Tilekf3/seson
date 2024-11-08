8673d0f98616b0989275606bf2bfef00e4536df8
Tilekf3 <tilekulanovich@gmail.com>
2024-11-08 16:19:25 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+let seson = prompt("напиши какой месяц")
+
+if (seson == 12) { 
+    alert ("зима");
+} 
+if (seson == 2) { 
+    alert ("зима");
+} 
+if (seson == 1) { 
+    alert ("зима");
+} 
+if (seson == 3) { 
+    alert ("весна");
+} 
+if (seson == 4) { 
+    alert ("весна");
+} 
+if (seson == 5) { 
+    alert ("весна");
+} 
+if (seson == 6) { 
+    alert ("лето");
+} 
+if (seson == 7) { 
+    alert ("лето");
+} 
+if (seson == 8) { 
+    alert ("лето");
+} 
+if (seson == 9) { 
+    alert ("осень");
+} 
+if (seson == 10) { 
+    alert ("осень");
+} 
+if (seson == 11) { 
+    alert ("осень");
+} 
+if (seson > 12) {
+    alert("error")
+
+}
+
+
+
+
+
+
+
+
+
+
